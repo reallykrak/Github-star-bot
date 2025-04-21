524d713435284045d733a0fec3bd35cc1fbcf7c0
krak <notreallykrak@gmail.com>
2025-04-21 18:22:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,1 @@
-# config.py
 
-# Hedef repo bilgileri
-repo_owner = "hasbutcu"  # Hedef repo sahibinin kullanıcı adı
-repo_name = "vsc-genel-bot"  # Hedef repo adı
-
-# GitHub Personal Access Token'lar (2 sahte hesap için)
-tokens = [
-    "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",  # Token1 (sahte hesap 1 için)
-    "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"   # Token2 (sahte hesap 2 için)
-]
