64b7c03cde0b26c7de4664824dbbfb85790efc33
krak <notreallykrak@gmail.com>
2025-04-21 18:24:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,18 @@
+# GitHub Token
+GITHUB_TOKEN = "ghp_YOUR_GITHUB_TOKEN"
 
+# GitHub Kullanıcı Adı
+GITHUB_USERNAME = "YOUR_GITHUB_USERNAME"
+
+# Proxy ayarları
+PROXIES = [
+    "http://proxy1_ip:port",
+    "http://proxy2_ip:port",
+    # Dilediğiniz kadar proxy ekleyebilirsiniz
+]
+
+# 2Captcha API Key (Captcha çözme servisi)
+CAPTCHA_API_KEY = "your_2captcha_api_key"
+
+# Hedef GitHub Projesinin URL'si
+REPO_URL = "Hedf github proje URL"
